bc1aed2d38b8bd73678f12f78e47ed13c964ae14
Will Foran <will.foran@gmail.com>
2020-02-18 21:47:26 -0500

    🎨 interpolate instead of explicit (APPNAME, template); gen key

--- create-app.sh
+++ create-app.sh
@@ -1,27 +1,55 @@
-#!/bin/sh -e
+#!/usr/bin/bash
+set -eou pipefail
+trap 'e=$?; [ $e -gt 0 ] && echo "$0 exited in error $e"' EXIT
+
+
+# defaults
+DEVICE=square-watch
+MIN_SDK=2.3.1
+DEVELOPER_KEY=$HOME/passwd/garmin/developer_key.der
+
+[ $# -lt 1 ] &&
+   echo "USAGE: $(basename $0) path/name [device=$DEVICE] [min_sdk=$MIN_SDK] [key=$DEVELOPER_KEY]" &&
+   exit 1
+
+# check we can find SDK, xml
+CONNECTIQ_HOME=$(dirname $(which monkeyc))
+[ -z "$CONNECTIQ_HOME" ] &&
+   echo "ERROR: cannot find Connect IQ SDK" &&
+   exit 1
+! which xml >/dev/null 2>&1 && echo "ERROR: missing 'xmlstarlet'. get with brew/apt/pacman" && exit 1
 
 # APP PARAMS
-APP_NAME=$1
-PATH_APP=$2
-COMPLETE_PATH=$2/$1
-DEVICE=$3
-[ "$DEVICE" = "" ] && DEVICE=fr920xt
-MIN_SDK=$4
-[ "$MIN_SDK" = "" ] && MIN_SDK=1.2.1
+COMPLETE_PATH="$1"; shift
+[ $# -ge 1 ] && DEVICE="$1" && shift
+[ $# -ge 1 ] && MIN_SDK="$1" &&  shift
+[ $# -ge 1 ] && DEVELOPER_KEY="$1" && shift
+
+# should create once. if app dir exists, bail.
+test -d $COMPLETE_PATH && echo "ERROR: app path already exists '$COMPLETE_PATH'" && exit 1
+
+# make the kye if we don't have it
+if [ ! -r $DEVELOPER_KEY ]; then
+   echo "WARNING: no key '$DEVELOPER_KEY'; making"
+   keydir="$(dirname "$DEVELOPER_KEY")"
+   keyname="$(basename "$DEVELOPER_KEY" .der)" 
+   [ ! -d $keydir ] && mkdir -p $keydir
+   pemkey=$keydir/$keyname
+   openssl genrsa -out $pemkey 4096
+   openssl pkcs8 -topk8 -inform PEM -outform DER -in $pemkey -out $DEVELOPER_KEY -nocrypt
+fi
+
 #APP_PREFIXE=$(echo $APP_NAME | tr '[:upper:]' '[:lower:]' | tr -d '-')
+APP_NAME="$(basename "$COMPLETE_PATH")"
 APP_PREFIXE="${APP_NAME}"
 APP_ENTRY="${APP_PREFIXE}App"
 APP_DELEGATE="${APP_PREFIXE}Delegate"
 APP_VIEW="${APP_PREFIXE}View"
 APP_MENU_DELEGATE="${APP_PREFIXE}MenuDelegate"
 
-# PRIVATE KEY
-DEVELOPER_KEY=$4
-[ "$DEVELOPER_KEY" = "" ] && DEVELOPER_KEY=/Users/alick/developer_key.der
-#[ "$DEVELOPER_KEY" = "" ] && DEVELOPER_KEY=/Path/to/key.der
 
 # WATCH-APP TEMPLATE
-cp -r $CONNECTIQ_HOME/bin/templates/watch-app/simple/ $PATH_APP/$APP_NAME
+cp -r $CONNECTIQ_HOME/templates/watch-app/simple/ $COMPLETE_PATH/
 
 # APP RESOURCE
 RESOURCE_PATH=$(find $COMPLETE_PATH -path "$COMPLETE_PATH/resources*.xml" | xargs | tr ' ' ':')
@@ -75,4 +103,4 @@ sed -i -e "s!\${AppName}!$APP_NAME!g" ${COMPLETE_PATH}/run.sh
 # CLEAN
 rm -f ${COMPLETE_PATH}/*.sh-e ${COMPLETE_PATH}/resources/strings/*.xml-e ${COMPLETE_PATH}/source/*.mc-e
 
-monkeyc -o ${COMPLETE_PATH}/bin/${APP_NAME}.prg -d ${DEVICE} -m ${COMPLETE_PATH}/manifest.xml -z ${RESOURCE_PATH} ${COMPLETE_PATH}/source/*.mc -w -y ${DEVELOPER_KEY}
+monkeyc -o ${COMPLETE_PATH}/${APP_NAME}.prg -d ${DEVICE} -m ${COMPLETE_PATH}/manifest.xml -z ${RESOURCE_PATH} ${COMPLETE_PATH}/source/*.mc -w -y ${DEVELOPER_KEY}
